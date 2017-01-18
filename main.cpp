eae2f16cc6ffce87b9d9be1515d38031d47a0598
meodorewan <dongnguyen1308@gmail.com>
2017-01-18 22:14:51 +0700

    them cap nhat mui

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 ofstream flog;                                          // file log
 string file_name;                                       // ten file log va file out
-int number_of_round;                                    // so round kien chay
-int ant_per_round;                                      // so kien chay 1 round
+int n_round;                                    // so round kien chay
+int n_ant;                                      // so kien chay 1 round
 int time_limit;                                         // gioi han thoi gian chay
 int reset_count_down;                                   // so lan kq can lap de reset mui
 double alpha;                                           // alpha
@@ -19,47 +19,42 @@ int k;                                                  // so luong huong duoc l
 string s;                                               // xau nhap vao
 int n;                                                  // do dai xau
 const string AA = "CMFILVWYAGTSQNEDHRKP";               // xau bieu dien cac amino acid
-vector<double> T[MAXN];                                 // ma tran mui
+double **pheromone;                                 // ma tran mui
 int x0[12] = { 1,-1,-1, 0, 1,-1, 1, 1, 0, 0,-1, 0};
 int y0[12] = { 1,-1, 1, 1, 0, 0,-1, 0, 1,-1, 0,-1};
 int z0[12] = { 0, 0, 0,-1, 1,-1, 0,-1, 1,-1, 1, 1};
 
 const double MJ_ENERGY[20][20] = {                      //ma tran MJ energy
-    {-1.06, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0.19, 0.04, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {-0.23, -0.42, -0.44, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0.16, -0.28, -0.19, -0.22, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {-0.08, -0.2, -0.3, -0.41, -0.27, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0.06, -0.14, -0.22, -0.25, -0.29, -0.29, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0.08, -0.67, -0.16, 0.02, -0.09, -0.17, -0.12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0.04, -0.13, 0, 0.11, 0.24, 0.02, -0.04, -0.06, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0.25, 0.03, -0.22, -0.01, -0.1, -0.09, 0.09, -0.13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {-0.08, 0.19, 0.38, 0.25, 0.23, 0.16, 0.18, 0.14, -0.07, -0.38, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0.19, 0.19, 0.31, 0.14, 0.2, 0.25, 0.22, 0.13, -0.09, -0.26, 0.03, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {-0.02, 0.14, 0.29, 0.21, 0.25, 0.18, 0.34, 0.09, -0.06, -0.16, -0.08, 0.2, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0.05, 0.46, 0.49, 0.36, 0.26, 0.24, 0.08, -0.2, 0.08, -0.06, -0.14, -0.14, 0.29, 0, 0, 0, 0, 0, 0, 0},
-    {0.13, 0.08, 0.18, 0.53, 0.3, 0.5, 0.06, -0.2, 0.28, -0.14, -0.11, -0.14, -0.25, -0.53, 0, 0, 0, 0, 0, 0},
-    {0.69, 0.44, 0.27, 0.35, 0.43, 0.34, 0.29, -0.1, 0.26, 0.25, 0, -0.26, -0.17, -0.32, -0.03, 0, 0, 0, 0, 0},
-    {0.03, 0.65, 0.39, 0.59, 0.67, 0.58, 0.24, 0, 0.12, -0.22, -0.29, -0.31, -0.17, -0.3, -0.15, 0.04, 0, 0, 0, 0},
-    {-0.19, 0.99, -0.16, 0.49, 0.16, 0.19, -0.12, -0.34, 0.34, 0.2, -0.19, -0.05, -0.02, -0.24, -0.45, -0.39, -0.29, 0, 0, 0},
-    {0.24, 0.31, 0.41, 0.42, 0.35, 0.3, -0.16, -0.25, 0.43, -0.04, -0.35, 0.17, -0.52, -0.14, -0.74, -0.72, -0.12, 0.11, 0, 0},
-    {0.71, 0, 0.44, 0.36, 0.19, 0.44, 0.22, -0.21, 0.14, 0.11, -0.09, -0.13, -0.38, -0.33, -0.97, -0.76, 0.22, 0.75, 0.25, 0},
+    {-1.06, 0.19, -0.23, 0.16, -0.08, 0.06, 0.08, 0.04, 0, -0.08, 0.19, -0.02, 0.05, 0.13, 0.69, 0.03, -0.19, 0.24, 0.71, 0},
+    {0.19, 0.04, -0.42, -0.28, -0.2, -0.14, -0.67, -0.13, 0.25, 0.19, 0.19, 0.14, 0.46, 0.08, 0.44, 0.65, 0.99, 0.31, 0, -0.34},
+    {-0.23, -0.42, -0.44, -0.19, -0.3, -0.22, -0.16, 0, 0.03, 0.38, 0.31, 0.29, 0.49, 0.18, 0.27, 0.39, -0.16, 0.41, 0.44, 0.2},
+    {0.16, -0.28, -0.19, -0.22, -0.41, -0.25, 0.02, 0.11, -0.22, 0.25, 0.14, 0.21, 0.36, 0.53, 0.35, 0.59, 0.49, 0.42, 0.36, 0.25},
+    {-0.08, -0.2, -0.3, -0.41, -0.27, -0.29, -0.09, 0.24, -0.01, 0.23, 0.2, 0.25, 0.26, 0.3, 0.43, 0.67, 0.16, 0.35, 0.19, 0.42},
+    {0.06, -0.14, -0.22, -0.25, -0.29, -0.29, -0.17, 0.02, -0.1, 0.16, 0.25, 0.18, 0.24, 0.5, 0.34, 0.58, 0.19, 0.3, 0.44, 0.09},
+    {0.08, -0.67, -0.16, 0.02, -0.09, -0.17, -0.12, -0.04, -0.09, 0.18, 0.22, 0.34, 0.08, 0.06, 0.29, 0.24, -0.12, -0.16, 0.22, -0.28},
+    {0.04, -0.13, 0, 0.11, 0.24, 0.02, -0.04, -0.06, 0.09, 0.14, 0.13, 0.09, -0.2, -0.2, -0.1, 0, -0.34, -0.25, -0.21, -0.33},
+    {0, 0.25, 0.03, -0.22, -0.01, -0.1, -0.09, 0.09, -0.13, -0.07, -0.09, -0.06, 0.08, 0.28, 0.26, 0.12, 0.34, 0.43, 0.14, 0.1},
+    {-0.08, 0.19, 0.38, 0.25, 0.23, 0.16, 0.18, 0.14, -0.07, -0.38, -0.26, -0.16, -0.06, -0.14, 0.25, -0.22, 0.2, -0.04, 0.11, -0.11},
+    {0.19, 0.19, 0.31, 0.14, 0.2, 0.25, 0.22, 0.13, -0.09, -0.26, 0.03, -0.08, -0.14, -0.11, 0, -0.29, -0.19, -0.35, -0.09, -0.07},
+    {-0.02, 0.14, 0.29, 0.21, 0.25, 0.18, 0.34, 0.09, -0.06, -0.16, -0.08, 0.2, -0.14, -0.14, -0.26, -0.31, -0.05, 0.17, -0.13, 0.01},
+    {0.05, 0.46, 0.49, 0.36, 0.26, 0.24, 0.08, -0.2, 0.08, -0.06, -0.14, -0.14, 0.29, -0.25, -0.17, -0.17, -0.02, -0.52, -0.38, -0.42},
+    {0.13, 0.08, 0.18, 0.53, 0.3, 0.5, 0.06, -0.2, 0.28, -0.14, -0.11, -0.14, -0.25, -0.53, -0.32, -0.3, -0.24, -0.14, -0.33, -0.18},
+    {0.69, 0.44, 0.27, 0.35, 0.43, 0.34, 0.29, -0.1, 0.26, 0.25, 0, -0.26, -0.17, -0.32, -0.03, -0.15, -0.45, -0.74, -0.97, -0.1},
+    {0.03, 0.65, 0.39, 0.59, 0.67, 0.58, 0.24, 0, 0.12, -0.22, -0.29, -0.31, -0.17, -0.3, -0.15, 0.04, -0.39, -0.72, -0.76, 0.04},
+    {-0.19, 0.99, -0.16, 0.49, 0.16, 0.19, -0.12, -0.34, 0.34, 0.2, -0.19, -0.05, -0.02, -0.24, -0.45, -0.39, -0.29, -0.12, 0.22, -0.21},
+    {0.24, 0.31, 0.41, 0.42, 0.35, 0.3, -0.16, -0.25, 0.43, -0.04, -0.35, 0.17, -0.52, -0.14, -0.74, -0.72, -0.12, 0.11, 0.75, -0.38},
+    {0.71, 0, 0.44, 0.36, 0.19, 0.44, 0.22, -0.21, 0.14, 0.11, -0.09, -0.13, -0.38, -0.33, -0.97, -0.76, 0.22, 0.75, 0.25, 0.11},
     {0, -0.34, 0.2, 0.25, 0.42, 0.09, -0.28, -0.33, 0.1, -0.11, -0.07, 0.01, -0.42, -0.18, -0.1, 0.04, -0.21, -0.38, 0.11, 0.26}
 };
 
 
+ /*
 namespace random_picker
 {
     int get_rand (int lim) // get a random number in 0..lim-1
     {
         seed = (12345LL * seed + 67890) % (1LL << 31);
         return seed % lim;
-    }
-
-    double get_rand () // get a random number in range [0,1]
-    {
-        seed = (12345LL * seed + 67890) % (1LL << 31);
-        return double(seed) / INT_MAX;
     }
 
     int pick (int sz, double w[]) // pick a random element from 0..sz-1, w[i]/sum(w) is the chance of i being picked
@@ -71,6 +66,7 @@ namespace random_picker
         return min(i, sz - 1);
     }
 };
+ */
 
 struct OutputStream          // synced output stream for standard output and log output
 {
@@ -88,8 +84,8 @@ namespace parser
     void set_default_parameters ()
     {
         file_name               = "4RXN";
-        number_of_round         = 500;
-        ant_per_round           = 100;
+        n_round         = 500;
+        n_ant           = 100;
         time_limit              = INT_MAX;
         reset_count_down        = 10;
         alpha                   = 1.0;
@@ -108,8 +104,8 @@ namespace parser
         for (int i = 1; i < argc; i += 2)
         {
             if (strcmp(argv[i], "-inst" ) == 0) file_name               = argv[i + 1];
-            if (strcmp(argv[i], "-r"    ) == 0) number_of_round         = atoi(argv[i + 1]);
-            if (strcmp(argv[i], "-ant"  ) == 0) ant_per_round           = atoi(argv[i + 1]);
+            if (strcmp(argv[i], "-r"    ) == 0) n_round         = atoi(argv[i + 1]);
+            if (strcmp(argv[i], "-ant"  ) == 0) n_ant           = atoi(argv[i + 1]);
             if (strcmp(argv[i], "-tl"   ) == 0) time_limit              = atoi(argv[i + 1]) * CLOCKS_PER_SEC;
             if (strcmp(argv[i], "-reset") == 0) reset_count_down        = atoi(argv[i + 1]);
             if (strcmp(argv[i], "-a"    ) == 0) alpha                   = atof(argv[i + 1]);
@@ -128,18 +124,18 @@ namespace parser
         ostringstream seed_str, k_str;
         k_str << k;
         seed_str  << seed;
-        cout << "DONE";
+        cout << "DONE ";
         // preparing outputs
         ios::sync_with_stdio(false);
         flog.open((file_name + "_" + k_str.str() +"_"+ seed_str.str() + ".log").c_str());
 
         // printing current settings
-        fout << "ACO for Protein Structure PredictionProblem\n";
+        fout << "ACO for Protein Structure Prediction Problem\n";
         fout << "Test                           : " << file_name                                            << "\n";
 
         fout << "\nParameter-settings\n";
-        fout << "number_of_round                : " << number_of_round                                      << "\n";
-        fout << "ant_per_round                  : " << ant_per_round                                        << "\n";
+        fout << "n_round                : " << n_round                                      << "\n";
+        fout << "n_ant                  : " << n_ant                                        << "\n";
         fout << "time_limit                     : " << time_limit / CLOCKS_PER_SEC                          << " (s)\n";
         fout << "reset_count_down               : " << reset_count_down                                     << "\n";
         fout << "alpha                          : " << alpha                                                << "\n";
@@ -209,25 +205,44 @@ namespace Local_search
 
 }
 
+struct Ant  {
+    int tour[n];
+    double i_best;
+    double g_best;
+} *ant_population;
+
 namespace ACO
 {
     double Tmin,Tmax;
     int MAXT;
     //Solution Gbest, Ibest;
 
-    void reset_T()
+    void evaporation()
     {
-
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++){
+                pheromone[i][j] = (1 - rho) * pheromone[i][j];
+            }
     }
 
-    void update_T()
-    {
-
+    /**
+    * update pheromone after an ant moved
+    * += 1/g_best
+    */
+    void update_pheromone(Ant ant) {
+        for (int i = 0; i < n - 1; i++) {
+            int h = ant.tour[i];
+            int r = ant.tour[i + 1];
+            pheromone[h][r] += 1/ant.g_best;
+        }
     }
 
-    void let_ant_run()
-    {
+    void let_ant_run(Ant ant){
+        for (int i = 0; i < n; i++) {
+            int choosen_way = 0;
+            double mx = 0;
 
+        }
     }
 
     void run()
@@ -235,14 +250,16 @@ namespace ACO
         Tmax = 1.0;
         Tmin = Tmax / 12 / n;
         MAXT = 1;
-        for (int i = 0; i < k; ++i)
-            MAXT *= 12;
-        for (int i = 0; i < n; ++i)
-            T[i] = vector<double> (MAXT,Tmax);
-        for (int rnd = 0; rnd < number_of_round; ++rnd)
+
+        pheromone = new double*[n];
+        for (int i = 0; i < n; i++)
+            pheromone[i] = new double[n];
+        ant_population = new Ant[n_ant];
+
+        for (int rnd = 0; rnd < n_round; ++rnd)
         {
-            for (int ant = 0; ant < ant_per_round; ++ant)
-                let_ant_run();
+            for (int i = 0; i < n_ant; ++i)
+                let_ant_run(ant_population[i]);
         }
     }
 }
